ab21b20209b96e418b42a5ea481514055ebc6143
Pranav <57974336+officialpranav@users.noreply.github.com>
2023-02-01 23:38:15 -0500

    Update sketch.js
    prevent page from moving when arrow keys are pressed

--- sketch.js
+++ sketch.js
@@ -91,3 +91,9 @@ function reset(){
   score = 5;
   loop();
 }
+
+window.addEventListener("keydown", function(e) {
+    if(["Space","ArrowUp","ArrowDown","ArrowLeft","ArrowRight"].indexOf(e.code) > -1) {
+        e.preventDefault();
+    }
+}, false);
